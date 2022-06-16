899c2cecc55746162f0e39d028dd195f63d714be
wendiia <77809849+wendiia@users.noreply.github.com>
2022-06-16 20:07:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,5 @@ from OrderSystem import OrderSystem
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    # QtGui.QFontDatabase.addApplicationFont('./GuiApp/Fonts/Microsoft_YaHei_UI_Light.ttc')
-    # QtGui.QFontDatabase.addApplicationFont('./GuiApp/Fonts/Rozovii_Chulok.ttf')
-    # QtGui.QFontDatabase.addApplicationFont('./GuiApp/Fonts/Segoe_Script.ttf')
     ui = OrderSystem()
     sys.exit(app.exec_())
