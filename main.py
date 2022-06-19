018920be99e5dc74520c3cbfc3dd381134ea223c
wendiia <77809849+wendiia@users.noreply.github.com>
2022-06-19 22:00:11 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtWidgets
-from OrderSystem import OrderSystem
-
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    ui = OrderSystem()
-    sys.exit(app.exec_())
