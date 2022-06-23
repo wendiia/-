98035aa87c6a975ef9721b49e2952ae3d9c58423
wendiia <77809849+wendiia@users.noreply.github.com>
2022-06-23 17:32:18 +0300

    Add files via upload

--- SqlData/Database.py
+++ SqlData/Database.py
@@ -4,6 +4,9 @@ import os
 
 
 class Database:
+    """Класс Database создает базу данных, если она была не создана, наполняет ее шаблонными данными
+        Основная задача - применение запросов к базе данных CakrDb.db
+    """
     def __init__(self):
         self.max_date = ""
         self.min_date = ""
@@ -23,6 +26,8 @@ class Database:
             self.connection.close()
 
     async def orders_data(self):
+        """Составляет и выполняет запрос на взятие данных с таблицы orders
+        """
         query = "SELECT id_main, surname, name, phone, name_cake, date_begin, date_end, cost " \
                 "FROM cake INNER JOIN orders USING (id_cake)"
         async with aiosqlite.connect(self.db) as db:
@@ -30,6 +35,12 @@ class Database:
                 return await cursor.fetchall()
 
     async def save_data(self, data):
+        """Составляет и выполняет запрос на сохранение данных в таблицу orders
+            Parameters
+            ----------
+            data : list
+                список с данными из таблицы приложения
+        """
         query = """INSERT INTO orders (id_main, surname, name, phone, id_cake, date_begin, date_end)
                    VALUES (?, ?, ?, ?, ?, ?, ?)"""
         async with aiosqlite.connect(self.db) as db:
@@ -40,6 +51,16 @@ class Database:
             return cursor.rowcount
 
     async def list_ingredients(self, ingredient, min_date, max_date):
+        """Составляет и выполняет запрос на взятие суммарного количество ингредиентов по взятым параметрам
+            Parameters
+            ----------
+            ingredient : str
+                выбранный ингредиент из comboBox
+            min_date : str
+                ранняя дата из таблицы orders
+            max_date : str
+                поздняя дата из таблицы orders
+        """
         list_ingr = []
         data = [min_date, max_date]
         if ingredient == "Все ингредиенты":
@@ -61,25 +82,33 @@ class Database:
             return list_ingr
 
     async def all_money(self):
+        """Составляет и выполняет запрос на получение суммарной прибыли
+        """
         with open("./SqlData/sql_files/queries/get_all_money.sql", 'r') as sql_file:
             async with aiosqlite.connect(self.db) as db:
                 async with db.execute(sql_file.read()) as cursor:
                     return await cursor.fetchone()
 
     async def min_max_dates(self):
+        """Составляет и выполняет запрос на получение ранней и поздней дат
+        """
         async with aiosqlite.connect(self.db) as db:
             async with db.cursor() as cur:
                 await cur.execute("SELECT MIN(date_begin), MAX(date_begin) FROM orders")
                 return await cur.fetchone()
 
     async def get_ingredients(self):
+        """Составляет и выполняет запрос на получение всего спискса ингредиентов
+        """
         async with aiosqlite.connect(self.db) as db:
             async with db.cursor() as cur:
                 await cur.execute("SELECT name_ingr FROM ingredients")
                 return [" ".join(x) for x in await cur.fetchall()]
 
     async def cake_id(self):
-        """Формирование dict_cake_id, где {id_cake: name_cake}, из таблицы cake (cake_db)"""
+        """
+        Составляет и выполняет запрос формирование dict_cake_id, где {id_cake: name_cake}, из таблицы cake (cake_db)
+        """
         dict_cake_id = {}
         async with aiosqlite.connect(self.db) as db:
             async with db.execute("SELECT id_cake, name_cake FROM cake") as cursor:
@@ -88,6 +117,9 @@ class Database:
             return dict_cake_id
 
     async def last_id_orders(self):
+        """
+        Составляет и выполняет запрос получение последнего id из таблицы orders
+        """
         async with aiosqlite.connect(self.db) as db:
             async with db.execute("SELECT id_main FROM orders ORDER BY id_main DESC LIMIT 1") as cursor:
                 return await cursor.fetchone()
--- SqlData/__init__.py
+++ SqlData/__init__.py
@@ -1,2 +1,7 @@
+"""Пакет SqlData необходим для создания и заполения базы данных CakeDb.db
+    Основное назначение - создание экземпляра класса Database
+   modules:
+    Database
+"""
 from .Database import Database
 ex_db = Database()
